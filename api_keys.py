4d7e3189eb3a7cb5def928fdae92e8a7953de15b
Javlon <valiyevjavlon001@gmail.com>
2025-04-22 15:31:15 +0500

    Api keys available

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+VOYAGE_API_KEY = os.getenv("VOYAGE_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+TAHRIRCHI_API_KEY = os.getenv("TAHRIRCHI_API_KEY")
